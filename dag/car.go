f343644a62e4de36db841553e6e0cbf80ff30168
temi0x <temiadev@gmail.com>
2025-03-18 23:41:23 +0100

    tests: add tests for the CreateNewAdder func in the DAGService

--- dag/car.go
+++ dag/car.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"io/fs"
 
+	"github.com/goddhi/storacha-go/dag/adder"
 	"github.com/ipfs/boxo/blockservice"
 	blockstore "github.com/ipfs/boxo/blockstore"
 	"github.com/ipfs/boxo/ipld/merkledag"
@@ -14,8 +15,7 @@ import (
 	"github.com/ipfs/go-cid"
 	"github.com/ipfs/go-datastore"
 	"github.com/ipfs/go-datastore/sync"
-	gocar "github.com/ipld/go-car"
-	"github.com/goddhi/storacha-go/dag/adder"
+	gocar "github.com/ipld/go-car/v2"
 )
 
 func BuildCAR(ctx context.Context, file fs.File, path string, fsys fs.FS) (io.Reader, cid.Cid, error) {
@@ -23,42 +23,42 @@ func BuildCAR(ctx context.Context, file fs.File, path string, fsys fs.FS) (io.Re
 	bs := blockstore.NewBlockstore(sync.MutexWrap(ds))
 	bserv := blockservice.New(bs, nil)
 	dagService := merkledag.NewDAGService(bserv)
-	
+
 	add, err := adder.CreateNewAdder(ctx, dagService)
 	if err != nil {
 		return nil, cid.Undef, fmt.Errorf("creating adder: %w", err)
 	}
 	defer add.Close()
-	
+
 	rootCid, err := add.Add(file, path, fsys)
 	if err != nil {
 		return nil, cid.Undef, fmt.Errorf("adding to DAG: %w", err)
 	}
-	
+
 	buf := new(bytes.Buffer)
-	
+
 	var blocksToWrite []blocks.Block
 	seenBlocks := make(map[string]bool)
-	
+
 	var collectBlocks func(c cid.Cid) error
 	collectBlocks = func(c cid.Cid) error {
 		if seenBlocks[c.String()] {
 			return nil
 		}
 		seenBlocks[c.String()] = true
-		
+
 		nd, err := dagService.Get(ctx, c)
 		if err != nil {
 			return fmt.Errorf("getting node %s: %w", c, err)
 		}
-		
+
 		blk, err := blocks.NewBlockWithCid(nd.RawData(), c)
 		if err != nil {
 			return fmt.Errorf("creating block: %w", err)
 		}
-		
+
 		blocksToWrite = append(blocksToWrite, blk)
-		
+
 		for _, link := range nd.Links() {
 			err := collectBlocks(link.Cid)
 			if err != nil {
@@ -67,12 +67,12 @@ func BuildCAR(ctx context.Context, file fs.File, path string, fsys fs.FS) (io.Re
 		}
 		return nil
 	}
-	
+
 	err = collectBlocks(rootCid)
 	if err != nil {
 		return nil, cid.Undef, fmt.Errorf("collecting blocks: %w", err)
 	}
-	
+
 	carWriter := gocar.CarWriter{
 		DataWriter: buf,
 		Header: &gocar.CarHeader{
@@ -80,18 +80,18 @@ func BuildCAR(ctx context.Context, file fs.File, path string, fsys fs.FS) (io.Re
 			Version: 1,
 		},
 	}
-	
+
 	for _, blk := range blocksToWrite {
 		err := carWriter.Put(blk)
 		if err != nil {
 			return nil, cid.Undef, fmt.Errorf("writing block to CAR: %w", err)
 		}
 	}
-	
+
 	err = carWriter.Finalize()
 	if err != nil {
 		return nil, cid.Undef, fmt.Errorf("finalizing CAR writer: %w", err)
 	}
-	
+
 	return buf, rootCid, nil
 }
--- go.mod
+++ go.mod
@@ -5,11 +5,12 @@ go 1.23.6
 require (
 	github.com/ipfs/boxo v0.28.0
 	github.com/ipfs/go-block-format v0.2.0
+	github.com/ipfs/go-blockservice v0.5.2
 	github.com/ipfs/go-cid v0.5.0
 	github.com/ipfs/go-datastore v0.7.0
 	github.com/ipfs/go-ipld-format v0.6.0
 	github.com/ipfs/go-merkledag v0.11.0
-	github.com/ipld/go-car v0.6.2
+	github.com/ipld/go-car/v2 v2.14.2
 )
 
 require (
@@ -24,7 +25,6 @@ require (
 	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
 	github.com/ipfs/bbloom v0.0.4 // indirect
 	github.com/ipfs/go-bitfield v1.1.0 // indirect
-	github.com/ipfs/go-blockservice v0.5.2 // indirect
 	github.com/ipfs/go-ipfs-blockstore v1.3.1 // indirect
 	github.com/ipfs/go-ipfs-ds-help v1.1.1 // indirect
 	github.com/ipfs/go-ipfs-exchange-interface v0.2.1 // indirect
@@ -46,11 +46,14 @@ require (
 	github.com/multiformats/go-base32 v0.1.0 // indirect
 	github.com/multiformats/go-base36 v0.2.0 // indirect
 	github.com/multiformats/go-multibase v0.2.0 // indirect
+	github.com/multiformats/go-multicodec v0.9.0 // indirect
 	github.com/multiformats/go-multihash v0.2.3 // indirect
 	github.com/multiformats/go-varint v0.0.7 // indirect
 	github.com/opentracing/opentracing-go v1.2.0 // indirect
+	github.com/petar/GoLLRB v0.0.0-20210522233825-ae3b015fd3e9 // indirect
 	github.com/polydawn/refmt v0.89.0 // indirect
 	github.com/spaolacci/murmur3 v1.1.0 // indirect
+	github.com/whyrusleeping/cbor v0.0.0-20171005072247-63513f603b11 // indirect
 	github.com/whyrusleeping/cbor-gen v0.1.2 // indirect
 	github.com/whyrusleeping/chunker v0.0.0-20181014151217-fe64bd25879f // indirect
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
@@ -61,6 +64,7 @@ require (
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/crypto v0.33.0 // indirect
+	golang.org/x/exp v0.0.0-20250210185358-939b2ce775ac // indirect
 	golang.org/x/sync v0.11.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
 	golang.org/x/xerrors v0.0.0-20240903120638-7835f813f4da // indirect
